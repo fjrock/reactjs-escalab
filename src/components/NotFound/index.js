a1972042090f67855603b7da8e9d40a8cd1f71ca
fjrock <fj@live.cl>
2021-05-04 17:47:43 -0400

    4

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ButtonBack from '../Common/ButtonBack';
-import Container from '@material-ui/core/Container';
 import error from '../../assets/img/error.png';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+
 
 const useStyles = makeStyles({
   root: {
@@ -21,17 +18,6 @@ const useStyles = makeStyles({
   },
 });
 
-const styles = {
-  paperContainer: {
-      
-      backgroundImage: `url(${error})`,
-      backgroundPosition: 'center',
-      backgroundSize: 'auto',
-      backgroundRepeat: 'no-repeat',
-      width: '100vw',
-      height: '100vh'
-  }
-};
 
 const NotFound = () => {
   const classes = useStyles();
