9d0d68ddc786d29900e70aefc32b64c5ede54fe8
fjrock <fj@live.cl>
2021-05-04 18:10:03 -0400

    4

--- src/components/Common/ButtonBack.js
+++ src/components/Common/ButtonBack.js
@@ -4,9 +4,8 @@ import { useHistory } from "react-router-dom";
 
 const ButtonBack = () => {
     const history = useHistory();
-    const goBack = () => {history.goBack()}
+    const goBack = () => {history.push("/");}
     return(
-      
         <Button variant="contained" color="primary" onClick={() => { goBack() }}>Go Back</Button>
 )};
 
--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import ButtonBack from '../Common/ButtonBack';
-import error from '../../assets/img/error.png';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
-import CardMedia from '@material-ui/core/CardMedia';
-
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
   root: {
     width: '100vw',
-    backgroundPosition: 'center'
   },
   media: {
-    height: '60vh'
+    height: '100vh'
   },
 });
 
@@ -25,11 +24,11 @@ const NotFound = () => {
   return(
     <Card className={classes.root}>
       <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={error}
-        />
-
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            PAGE NOT FOUND
+          </Typography>
+        </CardContent>
       </CardActionArea>
       <CardActions>
       <ButtonBack/> 
