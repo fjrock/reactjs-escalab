583638dbf107cf7c1010bb160c22c5edc21707ae
fjrock <fj@live.cl>
2021-06-11 11:53:51 -0400

    11
    11

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { Suspense,lazy } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import LeaguesContextProvider from "./contexts/LeaguesContext";
 import TeamsContextProvider from "./contexts/TeamsContext";
 import TeamContextProvider from "./contexts/TeamContext";
-import Leagues from "./components/Leagues";
-import Teams from "./components/Teams";
-import TeamDetail from "./components/Teams/TeamDetail";
+//import TeamDetail from "./components/Teams/TeamDetail";
 import NotFound from "./components/NotFound";
 import Header from "./components/Common/Header";
 import Footer from "./components/Common/Footer";
+import Loading from "./components/Common/Loading";
 import "./assets/css/styles.css";
 import Home from "./components/Common/Home";
+const Leagues = lazy(() => import('./components/Leagues'));
+const Teams = lazy(() => import('./components/Teams'));
+const TeamDetail = lazy(() => import('./components/Teams/TeamDetail'));
+
 
 const App = () => (
   <BrowserRouter>
-   <Header />
+   <Header/>
+   <Suspense fallback={<div><Loading /></div>}>
     <Switch>
       <Route exact path="/">
       <Home/>
@@ -36,6 +40,8 @@ const App = () => (
       </Route>
       <Route component={NotFound} />
     </Switch>
+    </Suspense>
+
     <Footer />
   </BrowserRouter>
 );
